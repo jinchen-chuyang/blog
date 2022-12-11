ffefcb147ed810e29ad71609025e1470ffe9a713
chuyang <3244723050@qq.com>
2022-12-11 17:31:41 +0800

    add bat file

---
+++ push_page.bat
@@ -0,0 +1,27 @@
+:: 显示命令
+@echo on
+
+:: 切换到当前的bat的路径下
+cd %~dp0
+%~d0
+
+:: 重新构建
+del /f docs/.vuepress/dist
+call npm run docs:build
+
+:: 显示命令
+@echo on
+cd docs/.vuepress/dist
+
+:: 提交
+call git init
+call git add -A
+call git commit -m "deploy"
+call git push -f git@github.com:jinchen-chuyang/blog.git master:pages
+
+
+:: 延迟15秒退出
+timeout /t 10
+
+:: 打开网页
+start chrome "https://jinchen-chuyang.github.io/blog/chapter_000/"
