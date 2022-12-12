138e686f1c5da3ae0015210af760862d0c71a1ee
tgr <3244723050@qq.com>
2022-12-12 18:32:25 +0800

    cocos creator 空工程目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,24 +14,25 @@ module.exports = {
         ],
 		sidebar: [
 		    {
-                title: 'centos7.6运行ts服务器',
-                path: '/chapter_000/',
+                title: 'oops+tsprc',
+                path: '/000_oops/',
                 collapsable: true, // 是否折叠
                 children: [
-                    { title: "centos7.6搭建node最新环境", path: "/chapter_000/00_centos7.6搭建node最新环境" },
-					{ title: "centos7.6搭建nginx文件服务器", path: "/chapter_000/01_centos7.6搭建nginx文件服务器" },
-					{ title: "centos7.6安装mongodb", path: "/chapter_000/02_centos7.6安装mongodb" },
-					{ title: "cocos打包设定远程加载资源", path: "/chapter_000/03_cocos打包设定远程加载资源" },
-					{ title: "centos7.6使用nginx隐藏服务端口", path: "/chapter_000/04_centos7.6使用nginx隐藏服务端口" },
+                    { title: "centos7.6搭建node最新环境", path: "/000_oops/00_centos7.6搭建node最新环境" },
+					{ title: "centos7.6搭建nginx文件服务器", path: "/000_oops/01_centos7.6搭建nginx文件服务器" },
+					{ title: "centos7.6安装mongodb", path: "/000_oops/02_centos7.6安装mongodb" },
+					{ title: "cocos打包设定远程加载资源", path: "/000_oops/03_cocos打包设定远程加载资源" },
+					{ title: "centos7.6使用nginx隐藏服务端口", path: "/000_oops/04_centos7.6使用nginx隐藏服务端口" },
                 ]
             },
 			{
-                title: 'cocos creator采坑之路',
-                path: '/chapter_001/',
+                title: 'cocos-creator',
+                path: '/001_ccc/',
                 collapsable: true, // 是否折叠
                 children: [
-                    { title: "cocos creator 加载扩展库", path: "/chapter_001/00_ccc_加载扩展库" },
-					{ title: "cocos creator 安装依赖库", path: "/chapter_001/01_ccc_安装依赖库" },
+                    { title: "cocos creator 加载扩展库", path: "/001_ccc/00_ccc_加载扩展库" },
+					{ title: "cocos creator 安装依赖库", path: "/001_ccc/01_ccc_安装依赖库" },
+					{ title: "cocos creator 空工程目录", path: "/001_ccc/02_ccc_空工程目录" },
                 ]
             },
         ]
---
+++ docs/001_ccc/02_ccc_空工程目录.md
@@ -0,0 +1,25 @@
+---
+title: cocos creator 空工程目录
+author: trb
+date: '2022-12-12'
+---
+
+按自己习惯规范的空工程目录：
+```shell
+[root@VM-12-6-centos assets]# tree
+.
+|-- main.scene
+|-- resources
+|   |-- common
+|   |   |-- animation
+|   |   |-- audio
+|   |   |-- effect
+|   |   |-- material
+|   |   |-- mesh
+|   |   |-- prefab
+|   |   |-- skybox
+|   |   `-- texture
+|   |-- config
+|   `-- language
+`-- script
+```
